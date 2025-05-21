fb9b3987a65b170f20f43e4362d88b2f8064357d
Khalimuha <kalimuha2016@gmail.com>
2025-05-22 02:38:15 +0300

    Carousel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./Components/Header/Header";
+
 import "./App.css";
+import CarouselEffect from "./Components/Carousel/CarouselEffect";
 
 function App() {
   return (
     <>
       <Header />
+      <CarouselEffect/>
     </>
   );
 }
