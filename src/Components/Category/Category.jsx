fe2c21bbfb95412669ea7ef46f9e053e0a0e9e98
Khalimuha <kalimuha2016@gmail.com>
2025-05-23 11:10:16 +0300

    product page added

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -8,10 +8,10 @@ function Category(){
     
     <section className={classes.category_container}>  
      {
-    categoryInfos.map((Infos)=>(
-    <CategoryCard data ={Infos}/>
+    categoryInfos.map((Infos, index)=>{
+   return <CategoryCard key = {index} data ={Infos}/>
     
-    ))
+     })
     }
     </section>
   
