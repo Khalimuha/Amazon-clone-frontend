ce42c5b5be4fd26a8162671b9fbf6718f1b9c91b
Khalimuha <kalimuha2016@gmail.com>
2025-06-03 07:01:56 +0300

    css file updated.

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,23 +1,16 @@
-import React from 'react'
-import {categoryInfos} from "./CategoryFullInfo"
-import CategoryCard from './CategoryCard'
-import classes from "./category.module.css"
+import React from "react";
+import { categoryInfos } from "./CategoryFullInfo";
+import CategoryCard from "./CategoryCard";
+import classes from "./Category.module.css";
 
-function Category(){
+function Category() {
   return (
-    
-    <section className={classes.category_container}>  
-     {
-    categoryInfos.map((Infos, index)=>{
-   return <CategoryCard key = {index} data ={Infos}/>
-    
-     })
-    }
+    <section className={classes.category_container}>
+      {categoryInfos.map((Infos, index) => {
+        return <CategoryCard key={index} data={Infos} />;
+      })}
     </section>
-  
-    
-    
-  )
+  );
 }
 
-export default Category
+export default Category;
