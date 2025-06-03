f8704fd97fcfc446e72721b548a0c669eabde764
Khalimuha <kalimuha2016@gmail.com>
2025-06-03 07:08:37 +0300

    css file updated.

--- src/Components/Category/CategoryCard.jsx
+++ src/Components/Category/CategoryCard.jsx
@@ -1,22 +1,19 @@
+import classes from "./Category.module.css";
+import { Link } from "react-router-dom";
 
-import classes from "./category.module.css"
-import {Link} from "react-router-dom"
-
-function CategoryCard ({data}) {
+function CategoryCard({ data }) {
   console.log(data);
   return (
-    <div className ={classes.category}>
-    <Link to={`/category/${data.name}`}>
-     <span> 
-      <h3>{data?.title}</h3>
-     </span>
-     <img src= {data?.imgLink} alt="Product image"/>
-      <p> Shop now</p>
-
-    </Link>
-  
+    <div className={classes.category}>
+      <Link to={`/category/${data.name}`}>
+        <span>
+          <h3>{data?.title}</h3>
+        </span>
+        <img src={data?.imgLink} alt="Product image" />
+        <p> Shop now</p>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default CategoryCard
+export default CategoryCard;
