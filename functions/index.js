82cac2e0aff77e2bc5646024ddcb64ad9802bfae
Khalimuha <kalimuha2016@gmail.com>
2025-06-03 06:14:45 +0300

    axios updated.

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,3 @@
-// const { onRequest } = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// dotenv.config();
-// const stripe = require("stripe")(process.env.STRIPE_KEY);
-
-// const app = express();
-// app.use(cors({ origin: true }));
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     message: "Success !"
-//   });
-// });
-
-// app.post("/payment/create", async (req, res) => {
-//   const total = req.query.total;
-//   if (total > 0) {
-//     // console.log("payment received", total);
-//     // res.send(total);
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount: total,
-//       currency: "usd"
-//     });
-//     console.log(paymentIntent);
-//     res.status(201).json({
-//       clientSecret: paymentIntent.client_secret,
-//     });
-//   } else {
-//     res.status(403).json({
-//       message: "total must be greater than 0"
-//     });
-//   }
-// });
-
-// exports.api = onRequest(app);
-
-
-//.................................
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
@@ -66,12 +23,11 @@ app.post("/payment/create", async (req, res) => {
       currency: "usd"
     });
     res.status(201).json({
-      clientSecret: paymentIntent.client_secret,
+      clientSecret: paymentIntent.client_secret
     });
   } else {
     res.status(403).json({ message: "total must be greater than 0" });
   }
 });
 
-exports.api = onRequest(app); 
-
+exports.api = onRequest(app);
--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-23c81/us-central1/api"
-  // baseURL: "http://localhost:5000/"
+  // local instance of firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-23c81/us-central1/api"
+
+  // deployed version of amanzon server on render.com
+  baseURL: "https://amazon-api-deploy-i5uj.onrender.com/"
 });
 
 export { axiosInstance };
